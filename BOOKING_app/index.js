eeeb580687013261802595996c65f120ad848594
Juan David <juandavidcarrillogarcia@gmail.com>
2024-07-09 18:32:34 -0500

    Se trabajo proceso de los  middelwar y proceso de registro de hoteles y inicio de los usuarios mediante peticiones

--- BOOKING_app/index.js
+++ BOOKING_app/index.js
@@ -27,10 +27,32 @@ mongoose.connection.on("disconnected", () => {
 
 
 //MIDDLEWARES
+
+
+
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
-app.use("/api/users", authRoute);
-app.use("/api/hotels", authRoute);
-app.use("/api/rooms", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong !"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+
+    })
+})
+
+
+
+
+
 
 
 app.get("/", (req, res) => {
