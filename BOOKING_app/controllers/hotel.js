bcc5173a6bf3c3a22441cd20f01ff1d3ec180663
Juan David <juandavidcarrillogarcia@gmail.com>
2024-07-25 22:22:44 -0500

    Se trabajo en el archivo controllers/hotel.js

--- BOOKING_app/controllers/hotel.js
+++ BOOKING_app/controllers/hotel.js
@@ -42,9 +42,40 @@ export const getHotel = async (req, res, next) => {
 }
 
 export const getHotels = async (req, res, next) => {
+
+    const { featured, limit, ...otherFilters } = req.query; // Desestructuracion de Objetos
     try {
-        const hotels = await Hotel.find()
+
+
+        // Crear un objeto de filtros
+        let filters = { ...otherFilters };
+        // Si el filtro 'featured' está presente, agregarlo a los filtros
+        if (featured) {
+            filters.featured = featured === 'true'; // Convertir a booleano
+        }
+
+        // Convertir 'limit' a número
+        //const limitNumber = Number(limit);
+
+        // Agregar mensajes de registro
+        console.log('Filters:', filters);
+        //console.log('Limit:', limitNumber);
+
+        // Ejecutar la consulta con los filtros y límite
+        const hotels = await Hotel.find(req.query).limit(req.query.limit);
+        console.log(hotels)
+
+        res.status(200).json(hotels);
+
+
+        /*
+
+        const  parametos= req.query
+        const hotels = await Hotel.find(req.query).limit(3)
+        console.log(req.query);
+        console.log(hotels);
         res.status(200).json(hotels)
+        */
     } catch (err) {
         next(err);
 
@@ -66,18 +97,25 @@ export const countByCity = async (req, res, next) => {
 
 export const countByType = async (req, res, next) => {
 
-
-
     try {
-        const hotelCount = Hotel.countDocuments({ type: "hotel" })
+        const hotelCount = await Hotel.countDocuments({ type: "hotel" })
         const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
         const resortCount = await Hotel.countDocuments({ type: "resort" });
         const villaCount = await Hotel.countDocuments({ type: "villa" });
         const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+        /*
         const list = await Promise.all(cities.map(city => {
             return Hotel.countDocuments({ city: city })
         }))
-        res.status(200).json(list)
+        */
+        res.status(200).json([
+            { type: "hotel", count: hotelCount },
+            { type: "apartments", count: apartmentCount },
+            { type: "resorts", count: resortCount },
+            { type: "villas", count: villaCount },
+            { type: "cabins", count: cabinCount },
+
+        ]);
     } catch (err) {
         next(err);
 
