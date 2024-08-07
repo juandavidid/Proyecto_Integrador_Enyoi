a6fb7a1c019c48487ca1bb2a0f66607f25ff189e
Juan David <juandavidcarrillogarcia@gmail.com>
2024-08-06 21:05:35 -0500

    Se configuro el Panel Administrativo

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,14 +1,47 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
+import { userColumns } from "../../datatablesource.js";  //userRows
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
+import { useLocation } from 'react-router-dom';
+import useFetch from "../../hooks/useFetch"
+import { useEffect } from "react";
 
-const Datatable = () => {
-  const [data, setData] = useState(userRows);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+const Datatable = ({ columns }) => {
+
+
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  //console.log(path)
+
+  const [list, setList] = useState([]); // Linea de codigo nueva  nota: siempre colocar useState([])
+  console.log(list);
+  const { data, loading, error } = useFetch(`/api/${path}`);
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
+  //Linea de codigo Nueva
+  useEffect(() => {
+    setList(data)
+  }, [data])
+
+  //const [data, setData] = useState(userRows);
+
+  const handleDelete = async (id) => {
+
+    try {
+      await axios.delete(`/api/${path}/${id}`);
+      setList(list.filter((item) => item._id !== id));
+    } catch (err) {
+
+    }
+
+
+
+
   };
 
   const actionColumn = [
@@ -24,7 +57,7 @@ const Datatable = () => {
             </Link>
             <div
               className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
+              onClick={() => handleDelete(params.row._id)}
             >
               Delete
             </div>
@@ -36,18 +69,22 @@ const Datatable = () => {
   return (
     <div className="datatable">
       <div className="datatableTitle">
-        Add New User
-        <Link to="/users/new" className="link">
+        {path}
+        <Link to={`/${path}/new`} className="link">
           Add New
         </Link>
       </div>
       <DataGrid
         className="datagrid"
-        rows={data}
-        columns={userColumns.concat(actionColumn)}
+        rows={list}  // rows={data}   // Liena de codigo nueva
+        columns={columns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
+
+
+        getRowId={row => row._id}       // linea de codigo nueva
+
       />
     </div>
   );
--- src/datatablesource.js
+++ src/datatablesource.js
@@ -7,7 +7,7 @@ export const userColumns = [
     renderCell: (params) => {
       return (
         <div className="cellWithImg">
-          <img className="cellImg" src={params.row.img} alt="avatar" />
+          <img className="cellImg" src={params.row.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"} alt="avatar" />
           {params.row.username}
         </div>
       );
@@ -20,25 +20,77 @@ export const userColumns = [
   },
 
   {
-    field: "age",
-    headerName: "Age",
+    field: "country",
+    headerName: "Country",
     width: 100,
   },
   {
-    field: "status",
-    headerName: "Status",
-    width: 160,
-    renderCell: (params) => {
-      return (
-        <div className={`cellWithStatus ${params.row.status}`}>
-          {params.row.status}
-        </div>
-      );
-    },
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  },
+
+];
+
+
+export const hotelColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 150,
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 100,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 230,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
   },
 ];
 
+
+export const roomColumns = [
+  { field: "_id", headerName: "ID", width: 70 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 230,
+  },
+  {
+    field: "desc",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 100,
+  },
+  {
+    field: "maxPeople",
+    headerName: "Max People",
+    width: 100,
+  },
+];
+
+
+
 //temporary data
+/*
 export const userRows = [
   {
     id: 1,
@@ -121,3 +173,4 @@ export const userRows = [
     age: 65,
   },
 ];
+*/
