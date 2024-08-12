e0bf2dd1923b14ca5818cdb0ae132806ca9f1b4f
Juan David <juandavidcarrillogarcia@gmail.com>
2024-08-11 20:44:42 -0500

    se cambio URL de desarrollo a URL de produccion

--- BOOKING_client/vite.config.js
+++ BOOKING_client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:8800",
+        target: "https://proyecto-integrador-enyoi.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace('/^\/api/', ''),
